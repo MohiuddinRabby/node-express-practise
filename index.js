9d38deb3bca82f24c1864dda7d7b59701c70ff87
MohiuddinRabby <rabbymohiuddin@gmail.com>
2020-06-30 22:49:12 +0600

    cors added

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
 const port = 3005;
 //express.static set static dir
 app.use(express.static('public'))
+// cors use to open get ports publicly or securely(in other ways)
+app.use(cors());
 //
 app.get('/', (req, res) => {
     res.send('Hello express,nodemon installed');
@@ -22,6 +25,5 @@ app.get('/users/:id', (req, res) => {
     const userID = req.params.id;
     const userName = users[userID];
     res.send({userName,userID});
-
 })
 app.listen(port, () => console.log("listening to port: ${port}"));
