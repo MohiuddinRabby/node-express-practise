c01bfbf1d2249daa81d0e5d536b13b36665b61bb
MohiuddinRabby <rabbymohiuddin@gmail.com>
2020-06-30 21:59:34 +0600

    more example  added

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 3005;
+//express.static set static dir
 app.use(express.static('public'))
+//
 app.get('/', (req, res) => {
     res.send('Hello express,nodemon installed');
 });
@@ -11,5 +13,15 @@ app.get('/fruit', (req, res) => {
         price: [10, 15, 5, 4, 2]
     }
     res.send(fruit);
+})
+// app.get('/users/:id',(req,res)=>{
+//     console.log(req.params);
+// })
+const users = ['testUser1', 'testUser2', 'testUser3'];
+app.get('/users/:id', (req, res) => {
+    const userID = req.params.id;
+    const userName = users[userID];
+    res.send(userName);
+
 })
 app.listen(port, () => console.log("listening to port: ${port}"));
